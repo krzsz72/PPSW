af43cb8e991c93eb612d92f3c42d5ee5ac9c37d2
unknown <kpiziak@student.agh.edu.pl>
2025-04-09 14:37:50 +0200

    Testy PO lancuchy

--- PiziakTEST/Current/main.c
+++ PiziakTEST/Current/main.c
@@ -3,32 +3,18 @@
 
 #define NULL '\0'
 
-//int test;
-int ipin=0x10000; // pin P1.16
-int idelayCounter;
-
-void Delay(unsigned long mili){
-	unsigned long idelayCounter;
-	unsigned long idelayMax;
-	idelayMax = 1500000*mili/1000; //nawiasy psuja???
-	
-	for (idelayCounter = 0; idelayCounter<idelayMax; idelayCounter++){}; // wskazanie stopera: 1.0000125 dla idelayCounter = 1500000
-};
-
-
-
-
-
-
+char string[]="Ala ma kot\0a";
+char string2[254];
+enum CompResult TestOfCompare;
 
 void CopyString(char pcSource[], char pcDestination[]) {
-    unsigned char ucIndexCharToCopy = 0;
+    unsigned char ucCharCounter = 0;
    
-    for (ucIndexCharToCopy = 0; pcSource[ucIndexCharToCopy] != NULL; ucIndexCharToCopy++)
+    for (ucCharCounter = 0; pcSource[ucCharCounter] != NULL; ucCharCounter++)
     {
-        pcDestination[ucIndexCharToCopy] = pcSource[ucIndexCharToCopy];
+        pcDestination[ucCharCounter] = pcSource[ucCharCounter];
     }
-    pcDestination[ucIndexCharToCopy] = pcSource[ucIndexCharToCopy];
+    pcDestination[ucCharCounter] = NULL;
 
 };
 
@@ -38,10 +24,10 @@ enum CompResult {
 };
 
 enum CompResult eCompareString(char pcStr1[], char pcStr2[]) {
-    unsigned char ucComparedChar = 0;
-    for (ucComparedChar = 0; pcStr1[ucComparedChar] != NULL; ucComparedChar++)
+    unsigned char ucCharCounter = 0;
+    for (ucCharCounter = 0; pcStr1[ucCharCounter] != NULL; ucCharCounter++)
     {
-        if (pcStr1[ucComparedChar] != pcStr2[ucComparedChar])
+        if (pcStr1[ucCharCounter] != pcStr2[ucCharCounter])
         {
             return DIFFERENT;
         };
@@ -50,46 +36,35 @@ enum CompResult eCompareString(char pcStr1[], char pcStr2[]) {
 }
 
 void AppendString(char pcSourceStr[], char pcDestinationStr[]) {
-    unsigned char ucDestLen = 0;
-    for (ucDestLen = 0; pcDestinationStr[ucDestLen] != NULL; ucDestLen++)
+    unsigned char ucCharCounter = 0;
+    for (ucCharCounter = 0; pcDestinationStr[ucCharCounter] != NULL; ucCharCounter++)
     {
         //licznik dlugosci pcDestinationStr
     }
-    CopyString(pcSourceStr, pcDestinationStr + ucDestLen);
+    CopyString(pcSourceStr, pcDestinationStr + ucCharCounter);
 }
 
 void ReplaceCharactersInString(char pcString[],char cOldChar, char cNewChar) {
-    unsigned char ucIndexCharToReplace = 0;
-    for (ucIndexCharToReplace = 0; pcString[ucIndexCharToReplace] != NULL; ucIndexCharToReplace++) {
-        if (pcString[ucIndexCharToReplace] == cOldChar)
+    unsigned char ucCharCounter = 0;
+    for (ucCharCounter = 0; pcString[ucCharCounter] != NULL; ucCharCounter++) {
+        if (pcString[ucCharCounter] == cOldChar)
         {
-            pcString[ucIndexCharToReplace] = cNewChar;
+            pcString[ucCharCounter] = cNewChar;
         }
     }
 }
 
 
 int main(){
-	// 0000 0000 0000 0000 0001 0000 0000 0000
-	// 0x00010000
-	IO1DIR=ipin;
-	IO1SET=ipin;
-	IO1CLR=ipin;
-
-	//TestOfCopyString();
+	CopyString(string,string2);
 	
-	//test = 99;
-
-/*
+	TestOfCompare = eCompareString(string,string2);
 	
-	while(1){
-		IO1SET=ipin;
-		Delay(500);
+	AppendString(string,string2);
 	
-		IO1CLR=ipin;
-		idelayCounter=0;
-		Delay(500);
-		idelayCounter=0;
-
-}*/
+	ReplaceCharactersInString(string,'a','x');
+	
+	
+	
+	
 }
