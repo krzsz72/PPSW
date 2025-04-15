ddaf021a7c95588921f42bbd4f1955bde8ae9639
unknown <kpiziak@student.agh.edu.pl>
2025-04-16 01:53:30 +0200

    konwesjre 3 funckji 16.04.25

--- PO/konwersje/konwersje/konwersje.c
+++ PO/konwersje/konwersje/konwersje.c
@@ -1,24 +1,84 @@
 #include <stdio.h>
 #define NULL '\0'
+#define NIBBLE_BM 0xF
 
 void UIntToHexStr(unsigned int uiValue, char pcStr[]) {
-	unsigned char i;
-	for (i = 0; i < 4; i++)
+	unsigned char ucNibbleCounter;
+	unsigned int uiCurrentNibble = uiValue;
+	pcStr[0] = '0';
+	pcStr[1] = 'x';
+	for (ucNibbleCounter = 0; ucNibbleCounter < 4; ucNibbleCounter++)
 	{
-		pcStr[i] = (uiValue & 0xff);
-		printf("hex %x\n",pcStr[i]);
-		printf("znak %c  i=%i ||||  \n", pcStr[i],i);
-		uiValue = (uiValue >> 4);
-		printf("przes %x\n============\n", uiValue);
-
+		uiCurrentNibble = ((uiValue >> (4 * ucNibbleCounter) & NIBBLE_BM ));
+		if (uiCurrentNibble<10)
+		{
+			pcStr[5-ucNibbleCounter] = uiCurrentNibble + '0';
+		}
+		else
+		{
+			pcStr[5-ucNibbleCounter] = uiCurrentNibble - 10 + 'A';
+		}
 	}
-	pcStr[i+1] = NULL;
-	printf("wynik po forze: \n %i \n%s",uiValue, pcStr);
+	pcStr[6] = NULL;
+};
+
+void TestOfUIntToHexStr() {
+	char testString[7];
+	int testInt = 65000;
+	UIntToHexStr(testInt, testString);
+	printf("\n====================\nliczba: %i\nteststring: %s\n", testInt, testString);
+
+}
+
+enum Result {OK,ERROR};
+enum Result eHexStringToUInt(char pcStr[], unsigned int* puiValue) {
+	unsigned char ucNibbleCounter= 0;
+	if ( (pcStr[0] == '0') && (pcStr[1] == 'x') && (pcStr[2]!=NULL) && !(pcStr[6]))
+	{
+		for (ucNibbleCounter= 0; ucNibbleCounter< 4; ucNibbleCounter++, *puiValue=*puiValue<<4)
+		{
+			if ((pcStr[ucNibbleCounter+2]-'0')<10)
+			{
+				*puiValue |= pcStr[ucNibbleCounter+ 2]-'0';
+			}
+			else
+			{
+				*puiValue |= pcStr[ucNibbleCounter+ 2] - 'A'+10;
+			}
+		}
+		*puiValue = *puiValue >> 4;
+		return OK;
+	}
+	else
+	{
+		return ERROR;
+	}
+};
+
+void TestOfeHexStringToUInt() {
+	char input[] = "0xFDE8";
+	unsigned int output = 0;
+	printf("blad: %i\n", eHexStringToUInt(input, &output));
+	printf("output: %i", output);
 
 };
 
+void AppendUIntToString(unsigned int uiValue, char pcDestinationStr[]) {
+	unsigned char ucCharCounter = 0;
+	for (ucCharCounter = 0; pcDestinationStr[ucCharCounter] != NULL; ucCharCounter++)
+	{
+
+	}
+	UIntToHexStr(uiValue, pcDestinationStr+ucCharCounter);
+};
+
+void TestOfAppendUIntToString() {
+	int test1 = 16;
+	char str[254] = "Ala ma kota ";
+	AppendUIntToString(test1, str);
+	printf("test %s", str);
+};
 
 int main() {
-	char str[] = "0x0000";
-	UIntToHexStr(45067,str);
+	TestOfAppendUIntToString();
 };
