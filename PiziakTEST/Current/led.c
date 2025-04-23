b6094c780d5a2eaa1234e3129ac7d47240a42880
unknown <kpiziak@student.agh.edu.pl>
2025-04-23 14:05:24 +0200

    pop1

--- PiziakTEST/Current/led.c
+++ PiziakTEST/Current/led.c
@@ -1,10 +1,10 @@
 #include <LPC21xx.H>
 #include "led.h"
 
-#define LED0_bm (1<<16) //0x10000
-#define LED1_bm (1<<17) //0x20000
-#define LED2_bm (1<<18) //0x40000
-#define LED3_bm (1<<19) //0x80000
+#define LED0_bm (1<<16)
+#define LED1_bm (1<<17)
+#define LED2_bm (1<<18)
+#define LED3_bm (1<<19)
 
 void LedInit(){
 	IO1DIR|=LED0_bm|LED1_bm|LED2_bm|LED3_bm;
