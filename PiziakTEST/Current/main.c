555b6092ef9241d24d46c470c2c231de28ef9d3b
unknown <kpiziak@student.agh.edu.pl>
2025-06-03 23:56:45 +0200

    przerwania

--- PiziakTEST/Current/main.c
+++ PiziakTEST/Current/main.c
@@ -1,45 +1,57 @@
-#include <LPC21xx.H>
 #include "led.h"
 #include "keyboard.h"
-#include "timer.h"
-
-#define NULL '\0'
-
-//zadanie: 7.4
-//data: 03.06.2025
+#include "timer_interrupts.h"
 
 
-int debugVar;
 
-
-void DelayOneMili(){
-	unsigned long ulDelayCounter =0;
-	for(ulDelayCounter =0; ulDelayCounter <7500;ulDelayCounter++){};
-
+void Automat(void){
+	enum LedState{STOP,STEP_LEFT,STEP_RIGHT};
+	static enum LedState eLedState=STOP;
+	
+	static unsigned int uiStepCounter=0;
+	
+	  switch(eLedState){
+        case STOP:
+            if(eKeyboardRead()==BUTTON_0){
+            eLedState=STEP_LEFT;
+            }
+            if(eKeyboardRead()==BUTTON_2){
+            eLedState=STEP_RIGHT;
+            }
+        
+        break;
+        
+        case STEP_LEFT:
+          if(eKeyboardRead()==BUTTON_1){
+            eLedState=STOP;
+          }else{
+            LedStepLeft();
+            uiStepCounter++;        
+            }
+        break;
+        case STEP_RIGHT:
+          if(eKeyboardRead()==BUTTON_1){
+            eLedState=STOP;
+          }else{
+            LedStepRight();
+            uiStepCounter++;        
+            }
+        break;
+      
+					}
 };
 
-void Delay(unsigned long ulMili){
-	unsigned long uldelayCount=0;
-	for (uldelayCount = 0; uldelayCount<ulMili; uldelayCount++){
-		DelayOneMili();
-	}; // wskazanie stopera: 1.0000125 dla idelayCounter = 1500000
-};		// dla mikrokontorlera wartosc jest inna: 7500 ~= 1ms
-
-	enum LedState{STOP,STEP_LEFT,STEP_RIGHT,WIPER};
-	enum LedState eLedState=STOP;
+int main (){
 	
-	unsigned int uiStepCounter=0;
-	
-
-	int main(){
+	unsigned int iMainLoopCtr;
 	LedInit();	
 	KeyboardInit();
-	InitTimer0();
-	WaitOnTimer0(1000000);
-		
-	InitTimer0Match0(1000000);
-	WaitOnTimer0Match0();
-		
-};
 
+	Timer0Interrupts_Init(20000,&Automat);
+	
 
+	while(1){
+	 	iMainLoopCtr++;
+		
+	}
+}
---
+++ PiziakTEST/Current/timer_interrupts.h
@@ -0,0 +1,6 @@
+
+
+__irq void Timer0IRQHandler(void);
+
+
+void Timer0Interrupts_Init(unsigned int uiPeriod, void (*ptrInterruptFunction)(void));
