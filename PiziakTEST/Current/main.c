bebe6573711db671b4caf7b257c17f67ca1d1cc7
unknown <kpiziak@student.agh.edu.pl>
2025-06-04 15:12:24 +0200

    zdane timery

--- PiziakTEST/Current/main.c
+++ PiziakTEST/Current/main.c
@@ -9,33 +9,12 @@
 //data: 03.06.2025
 
 
-int debugVar;
-
-
-void DelayOneMili(){
-	unsigned long ulDelayCounter =0;
-	for(ulDelayCounter =0; ulDelayCounter <7500;ulDelayCounter++){};
-
-};
-
-void Delay(unsigned long ulMili){
-	unsigned long uldelayCount=0;
-	for (uldelayCount = 0; uldelayCount<ulMili; uldelayCount++){
-		DelayOneMili();
-	}; // wskazanie stopera: 1.0000125 dla idelayCounter = 1500000
-};		// dla mikrokontorlera wartosc jest inna: 7500 ~= 1ms
-
-	enum LedState{STOP,STEP_LEFT,STEP_RIGHT,WIPER};
-	enum LedState eLedState=STOP;
-	
-	unsigned int uiStepCounter=0;
-	
 
 	int main(){
 	LedInit();	
 	KeyboardInit();
 	InitTimer0();
-	WaitOnTimer0(1000000);
+	//WaitOnTimer0(1000000);
 		
 	InitTimer0Match0(1000000);
 	WaitOnTimer0Match0();
--- PiziakTEST/Current/timer.c
+++ PiziakTEST/Current/timer.c
@@ -8,35 +8,37 @@
 #define T0_IR_MR0_BM (1<<0)
 
 void InitTimer0(void){
-	T0TCR = T0_ENABLE_BM;
+	T0TCR = T0_ENABLE_BM; //ustawianie bitu wlaczenia timer0
 	};
 
 
 void WaitOnTimer0(unsigned int uiTime){
 
-	T0TCR |= T0_RESET_BM;
-	T0TCR &= ~(T0_RESET_BM);
+	T0TCR |= T0_RESET_BM; //ustawianie bitu resetu t0
+	T0TCR &= ~(T0_RESET_BM); // kasowanie bitu resetu t0
 	//1us
-	while(T0TC<=uiTime*15){};
+	while(T0TC<=uiTime*15){}; // dziala dopoki timer counter jest <= od ustawionego czasu
 	
 };
 
 void InitTimer0Match0(unsigned int iDelayTime){
 	
-	T0MCR|=T0MCR_INTERRUPT_BM;
-	T0MCR|=T0MCR_RESET_BM;
-	T0MR0=iDelayTime*15;
+	T0MCR|=T0MCR_INTERRUPT_BM; //ustawienie zwracania flagi interrupt przez MR
+	T0MCR|=T0MCR_RESET_BM; // zwracanie flagi reset przez MR
+	T0MR0=iDelayTime*15; // wpisane wartosci dla ktorej MR0 sie zalaczy
 	
-	T0TCR |= T0_RESET_BM;
-	T0TCR &= ~(T0_RESET_BM);
-	T0TCR = T0_ENABLE_BM;
+	T0TCR |= T0_RESET_BM; // bit resetu
+	T0TCR &= ~(T0_RESET_BM); // kasowanie bitu resetu
+	T0TCR = T0_ENABLE_BM; //bit wlaczajacy
 	
 };
 
 void WaitOnTimer0Match0(){
 
-	while((T0IR&T0_IR_MR0_BM)==0){};
-	T0IR = T0_IR_MR0_BM;
+	while((T0IR&T0_IR_MR0_BM)==0){}; // oczekiwanie na flage interrupt
+	
+		T0IR = (1<<3);
+		T0IR |= T0_IR_MR0_BM; //usuwanie flagi przerwania (wpisujac 1)
 
 	
 };
